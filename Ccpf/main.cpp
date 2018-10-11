2e96d36b4d355746c1978f71266dfd0261a4b505
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-10-11 07:46:28 -0300

    Update main.cpp

--- Ccpf/main.cpp
+++ Ccpf/main.cpp
@@ -4,49 +4,6 @@
 
 int main()
 {
-    //std::string x;
-    //std::getline(std::cin,x);
     Cpf::validacpf("7039107015");
 
-    /*
-    Cpf p;
-    std::string x;
-    int escolha;
-
-    do
-    {
-        p.menu();
-        std::cin>>escolha;
-
-        switch (escolha)
-        {
-            case 1:
-
-                std::cout<<"Digite o cpf para o mesmo ser validado!\n";
-                std::cin.ignore();
-                std::getline(std::cin,x);
-                if(Cpf::validacpf(&x))
-                {
-                    std::cout<<"\nCpf valido!\n";
-                }
-                else
-                {
-                    std::cout<<"\nCpf invalido!\n";
-                }
-                break;
-
-            case 2:
-
-            break;
-
-            default:
-
-                std::cout<<"\nDigite uma opção valida!\n";
-                break;
-        }
-
-    }while(escolha!=2);
-
-    return 0;
-    */
 }
